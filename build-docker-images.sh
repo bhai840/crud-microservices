931e7f50fc65e9a537799d078535b0a9257213dc
rochapaulo <almeida.paulorocha@gmail.com>
2017-03-06 02:35:16 -0300

    docker

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,7 +1,5 @@
 #!bin/sh
 
-mvn clean install crudmicroservices-edge
-docker build -t crudmicroservices-edge/crudmicroservices-edge:latest .
-
-mvn clean install crudmicroservices-middle
-docker build -t crudmicroservices-middle/crudmicroservices-middle:latest .
+mvn clean install -DskipTests -o
+docker build -t crudmicroservices-edge:latest crudmicroservices-edge/
+docker build -t crudmicroservices-middle:latest crudmicroservices-middle/
